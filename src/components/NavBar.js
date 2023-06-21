2890db26e5cf90ed6c313e7a4e499ff1114b667c
ceoraford <ceora.ford@okta.com>
2023-06-21 09:17:47 -0400

    Add basic styles for navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,17 @@
 import Link from "next/link";
+import NavBarStyles from "../styles/NavBar.module.css";
 
 export default function NavBar() {
   return (
     <>
-      <h1>NCT - The Course</h1>
-        <ul>
-          <li><Link href="/">Home</Link></li>
+      <nav>
+        <ul className={NavBarStyles.container}>
+          <li><Link href="/"><h1>NCT - The Course</h1></Link></li>
           <li><Link href="/about"> About </Link> </li>
           <li> <Link href="/courses"> Courses</Link> </li>
           <li><Link href="/requests">Requests</Link></li>
         </ul>
+      </nav>
     </>
   )
   }
