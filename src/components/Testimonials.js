496909a91a47ecf9c9affc5b9fe71a0b2b3977bc
ceoraford <ceora.ford@okta.com>
2023-06-22 13:47:39 -0400

    Create skeleton for testimonials

---
+++ src/components/Testimonials.js
@@ -0,0 +1,8 @@
+export default function Testimonials() {
+  return (
+    <div>
+      <h2> Testimonials </h2>
+      <p> Here what our users have to say about our incredible courses</p>
+    </div>
+  )
+}
