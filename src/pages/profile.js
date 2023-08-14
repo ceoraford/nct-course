174daec3eeef02433905eab523608f35785517ca
ceoraford <ceora.ford@okta.com>
2023-08-14 15:05:22 -0400

    Change image tag on profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,5 +1,6 @@
 import NavBar from "@/components/NavBar";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
 
 export default function Profile(){
   const { user, error, isLoading } = useUser();
@@ -11,7 +12,7 @@ export default function Profile(){
     user && (
       <div>
         <NavBar />
-        <img src={user.picture} alt={user.name} />
+        <Image src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
       </div>
